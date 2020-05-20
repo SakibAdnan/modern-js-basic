5a1f7d8ca46584fc65f5fb5fac684a7e5ac46024
Nazmun Sakib <wp.coder3@gmail.comm>
2020-05-20 00:13:22 -0700

    create var and datatpes of js

---
+++ datatypes.js
@@ -0,0 +1,10 @@
+var a = 'Hell'
+var b = 12
+var c = 1.76
+var d = null;
+var e = false;
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
---
+++ variables.js
@@ -0,0 +1,8 @@
+var name = 'Nazmun Sakib';
+var age = 23;
+
+console.log('Name : '+ name)
+console.log('Age : '+ age);
+
+console.log(name +' Knows Javasscript');
+console.log('His  age only '+ age);
