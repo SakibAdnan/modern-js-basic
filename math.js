05bd41dad1e084141095d583862468119441f85b
Nazmun Sakib <wp.coder3@gmail.comm>
2020-05-20 04:09:54 -0700

    Wirking with date fucntion and math fucntion

---
+++ math.js
@@ -0,0 +1,11 @@
+console.log(Math.E)
+console.log(Math.PI)
+var num = 5.667
+console.log(Math.abs(num));
+console.log(Math.floor(num))
+console.log(Math.ceil(num))
+console.log(Math.round(num));
+var num = 5.300
+console.log(Math.round(num))
+
+console.log( Math.round( Math.random() * 10 + 1 ))
