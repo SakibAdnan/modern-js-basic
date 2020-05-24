3eef21e6c5064c3ec87d42c314234c5b6469b6f3
Nazmun Sakib <wp.coder3@gmail.comm>
2020-05-23 22:58:21 -0700

    add  js loop

---
+++ loop.js
@@ -0,0 +1,62 @@
+//for loop
+for(var i =0; i<=100; i++ ){
+    //console.log(i); 
+}
+
+for(var i = 0; i <= 100; i++){
+    if(i%2 === 1){
+        //console.log(i + ' = Odd Number')
+    }
+}
+
+
+
+for(var i = 0; i <= 100; i++){
+    if(i%2 === 0){
+        //console.log(i + ' = Even Number')
+    }
+}
+
+var sum = 0;
+for(var i = 0; i <= 100; i++){
+    if(i%2 === 0){
+        //console.log(sum + ' + ' + i + ' = ' + (sum+i));
+    }
+    //sum++
+}
+//console.log(sum)
+
+var isWinner = 0;
+while(isWinner < 1){
+    var randomNum = Math.floor(Math.random() * 10 );
+    if(randomNum === 7 ){
+        console.log('Congratulation, You are Winner');
+    }else{
+        console.log('You are got ' + randomNum + ' Best wish for next time');
+    }
+    isWinner++
+}
+
+//Do While
+// do{
+//     Working code
+// }while('condition');
+
+//Nested loop
+var n = 5;
+for(var i = 1; i <= n; i++){
+    var string = ' ';
+    for(var j = 1; j <=i; j++){
+        string += j + ' '
+    }
+    console.log(string)
+}
+
+var n = 5;
+for(var i = 1; i <= n; i++){
+    var string = ' ';
+    for(var j = 1; j <=i; j++){
+        string += '* '
+    }
+    console.log(string)
+}
--- ternary.js
+++ ternary.js
@@ -5,3 +5,9 @@
 var num = 11;
 var result = num % 2 === 0 ? 'This is Even Number': 'This is Odd Number';
 console.log(result);
+
+//JS Short Hand
+
+var name = 'Rakib Hasan';
+var fullName = name || 'Sakib Adnan'
+console.log(fullName);
